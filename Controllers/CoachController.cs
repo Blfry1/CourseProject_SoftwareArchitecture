737f27da80b596e38d3ee3201b3a9c7730c324af
jazmenb92 <jazmenb92@gmail.com>
2022-02-22 23:01:24 -0600

    *Updated Sprint 2 coaches can now add their profile

--- Controllers/CoachController.cs
+++ Controllers/CoachController.cs
@@ -10,6 +10,7 @@ using System.Threading.Tasks;
 
 namespace CourseProject_SoftwareArchitecture.Controllers
 {
+    /* GIT HUB 2/22 11:00PM*/
     [Authorize(Roles = "Coach")]
 
     public class CoachController : Controller
