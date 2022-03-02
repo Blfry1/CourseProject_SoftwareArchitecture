423206faa3f72c3f9f981cbcc637d3ba375afbe1
jazmenb92 <jazmenb92@gmail.com>
2022-03-02 11:25:55 -0600

    Page styling

--- Startup.cs
+++ Startup.cs
@@ -1,5 +1,4 @@
 using CourseProject_SoftwareArchitecture.Models;
-using FluentAssertions.Common;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Identity;
